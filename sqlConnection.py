8c6ad2bde9633ef0fe12614570abcd52cb987411
Marius Schmiedt <schmiedt.marius@gmail.com>
2024-01-21 20:56:43 +0100

    updated sqlConnection

---
+++ sqlConnection.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+class sqlConnection():
+    def __init__(self, host='', user='', password='', database='', port=3306, autocommit=True):
+        self.connection = {
+            'host': host,
+            'user': user,
+            'password': password,
+            'database': database,
+            'port': port,
+            'autocommit': autocommit
+        }
+        self.mydb = None
+        self.cur = None
+        
+        self.openMySQL()
+    
+    def openMySQL(self):
+        self.mydb = mysql.connector.connect(
+            host=self.connection['host'],
+            user=self.connection['user'],
+            password=self.connection['password'],
+            database=self.connection['database'],
+            port=self.connection['port'],
+            autocommit=self.connection['autocommit']
+        )
+        
+        # set cursor
+        self.cur = self.mydb.cursor()
+    
+    def closeMySQL(self):
+        # close cursor
+        self.cur.close()
+        # disconnect from server
+        self.mydb.close()
