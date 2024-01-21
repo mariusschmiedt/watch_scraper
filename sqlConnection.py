ffdeecd7d2abf4c7b701095e8abda5b2e915d9d7
mariusschmiedt <101993014+mariusschmiedt@users.noreply.github.com>
2024-01-21 20:52:22 +0100

    Update sqlConnection.py

--- sqlConnection.py
+++ sqlConnection.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 class sqlConnection():
-    def __init__(self, host='srv988.hstgr.io', user='u205008851_smarius', password='Tm130119!', database='u205008851_watches', port=3306, autocommit=True):
+    def __init__(self, host='', user='', password='', database='', port=3306, autocommit=True):
         self.connection = {
             'host': host,
             'user': user,
